b38fe0114e14bd7fb6fa92063869c8eb1eebf1f8
dar <mendelowitz10@gmail.com>
2023-03-21 12:14:08 +0200

    requests

---
+++ PackageArrangementServer/Models/LoginRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace PackageArrangementServer.Models
+{
+    public class LoginRequest
+    {
+        public string Id { get; set; }
+        public string Password { get; set; }
+
+        public LoginRequest(string id, string password)
+        {
+            this.Id = id;
+            this.Password = password;
+        }
+    }
+}
--- PackageArrangementServer/Models/UserList.cs
+++ PackageArrangementServer/Models/UserList.cs
@@ -13,5 +13,25 @@
         }
 
         public List<User> Users { get { return _users; } } // need to fix - return a copy
+
+        public int Count { get { return _users.Count; } }
+
+        public void Add(User user)
+        {
+            if (user == null || _users.Contains(user)) return;
+            _users.Add(user);
+        }
+
+        public void Edit(User user, string? name = null, string? password = null)
+        {
+            if (user == null) return;
+            if (_users.Contains(user))
+            {
+                int index = _users.IndexOf(user);
+
+                if (name != null) _users[index].Name = name;
+                if (password != null) _users[index].Password = password;
+            }
+        }
     }
 }

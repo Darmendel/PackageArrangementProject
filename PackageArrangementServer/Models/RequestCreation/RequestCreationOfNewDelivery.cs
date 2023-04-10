eba8e268a644181d3587d277177f8bdf69d7d8ec
dar <mendelowitz10@gmail.com>
2023-04-10 18:12:40 +0300

    updated user controller

--- PackageArrangementServer/Models/RequestCreation/RequestCreationOfNewDelivery.cs
+++ PackageArrangementServer/Models/RequestCreation/RequestCreationOfNewDelivery.cs
@@ -2,6 +2,7 @@
 {
     public class RequestCreationOfNewDelivery
     {
+        public string UserId { get; set; }
         public DateTime DeliveryDate { get; set; }
         public List<Package> Packages { get; set; }
         public Container? Container { get; set; }

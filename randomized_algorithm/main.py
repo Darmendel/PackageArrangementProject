50477d3d2266b8c475ac41073bb74ce8e86aba4b
Jacksparrowking <72932653+Jacksparrowking@users.noreply.github.com>
2023-04-12 03:26:51 +0300

    Update main.py

--- randomized_algorithm/main.py
+++ randomized_algorithm/main.py
@@ -149,7 +149,7 @@ def start():
     # construction algorithm
     solution = construction_phase(pkgs=pkgs, cont=cont)
     # improvement algorithm:
-    impoved_solution = improvement_alg(pkgs=solution, cont_info=cont)
+    improved_soulution = improvement_alg(pkgs=solution, cont_info=cont)
 
 
 start()

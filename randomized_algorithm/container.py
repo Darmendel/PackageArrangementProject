d0dfd44496201c1d8f2476fb35fb9f555c1ed90c
Jacksparrowking <72932653+Jacksparrowking@users.noreply.github.com>
2023-04-14 04:35:28 +0300

    fix storage methods and add new methods.

--- randomized_algorithm/container.py
+++ randomized_algorithm/container.py
@@ -1,5 +1,11 @@
 # TODO change the weight limit to something reasonable.
+import numpy as np
+from storage import EfficientStorage
 
+import numpy as np
+
+
+# TODO: add total possible amount of items.
 class Container:
 
     def __init__(self, height: int, width: int, length: int, weight_limit: int = 5000):
@@ -8,7 +14,12 @@ class Container:
         self.length = length
         self.weight_limit = weight_limit
         self.volume = self.height * self.width * self.length
+        # self.used_x = EfficientStorage
+        # self.used_y =
+        # self.used_z =
+    # update weight & occupied space.
+    def update_container(self):
+        pass
 
-
-
-
+    def convert_to_json(self):
+        pass
